2b3a7c53faa4ce42f04324b9b2e0bba1d1d81110
Dinesh Kumar <dineshsoni8569@gmail.com>
2025-05-01 19:47:02 +0530

    useeffect and api call

--- src/components/RevisedDay.jsx
+++ src/components/RevisedDay.jsx
@@ -1,20 +1,68 @@
-import React, { useEffect,useState, useMemo } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 
-const RevisedDay = () => {
+function Counter() {
   const [count, setCount] = useState(0);
-  console.log("rendered");
-
-  
-
- 
-  
   return (
     <div>
       <p>count : {count}</p>
-      <p>{isEven ? "Even" : "Odd"}</p>
 
       <button onClick={() => setCount((prev) => prev + 1)}>+</button>
       <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+    </div>
+  );
+}
+
+function UserCard() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await res.json();
+        setUsers(data);
+        console.log(data);
+      } catch (error) {
+        console.log("error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getUsers();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Users loading....</p>
+      ) : (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              <span>
+                Name : {user.name} <br />
+                email: {user.email}
+              </span>
+              <br />
+              <span>website:{user.website}</span>
+              <br />
+              <span>city:{user.address.city}</span>
+              <br />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+const RevisedDay = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <Counter />
+      <UserCard />
     </div>
   );
 };
