f7b4b1c7edd748e3e72da6ab27e02e495a72def3
Dinesh Kumar <dineshsoni8569@gmail.com>
2025-05-02 21:31:21 +0530

    0205

--- src/components/date02/UserDetail.jsx
+++ src/components/date02/UserDetail.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const UserDetail = () => {
   const { id } = useParams();
 
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, [id]);
+
+  console.log(user);
   return (
     <div>
       UserDetail
       <p>User ID: {id} </p>
+      {user ? (
+        <div>
+          <p>Name: {user.name}</p>
+          <p>Email: {user.email}</p>
+          <p>Website: {user.website}</p>
+          <p>City: {user.address.city}</p>
+        </div>
+      ) : (
+        <p>Loading user...</p>
+      )}
     </div>
   );
 };
