1e177db35db49ba29755a9d6188d0b767c9d264c
Dinesh Kumar <dineshsoni8569@gmail.com>
2025-05-02 19:42:57 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-
 import "./App.css";
-import Learning from "./components/Learning";
 
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import Learning from "./components/Learning";
 
 function App() {
   return (
-    <div className="hero">
-      <Learning />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/learning">Learning</Link>
+      </nav>
+
+      <div className="hero">
+
+        
+        <Routes>
+          <Route path="/learning" element={<Learning />} />
+          <Route path="*" element={<p>No page found 404</p>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
