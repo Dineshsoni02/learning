52a721733cff1b2fb2ddfe40e03db1e1db1838c2
Dinesh Kumar <dineshsoni8569@gmail.com>
2025-05-02 20:33:30 +0530

    restructure

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import Learning from "./components/Learning";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
@@ -13,9 +14,8 @@ function App() {
       </nav>
 
       <div className="hero">
-
-        
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/learning" element={<Learning />} />
           <Route path="*" element={<p>No page found 404</p>} />
         </Routes>
